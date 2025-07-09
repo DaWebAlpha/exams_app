35de433b2d447ffbdde9701abb8bbeb7d536c8eb
DaWebAlpha <tribillio@gmail.com>
2025-07-09 12:22:10 -0700

    exams

--- config/db.js
+++ config/db.js
@@ -5,6 +5,9 @@ dotenv.config()
 
 const MONGO_URI = process.env.MONGO_URI
 
-mongoose.connect(MONGO_URI)
-
-export default mongoose
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("✅ MongoDB connected"))
+.catch(err => console.error("❌ MongoDB connection error:", err));
